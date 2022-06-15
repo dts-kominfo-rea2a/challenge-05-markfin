63001b9141f5d54237675e287766716087f004c5
markfin <marfinzamarudin@gmail.com>
2022-06-15 21:06:29 +0900

    menambahkan data

--- index.js
+++ index.js
@@ -6,25 +6,36 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (name, callback) => {
+    const nameSorting = callback(name)
+    const containerSorter = []
+    for (index = 0; index < nameSorting.length; index++) {
+        containerSorter.push(`${index+1}. ${nameSorting[index]}`)
+    }
+
+    return containerSorter
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
-
-// TODO: Fungsi untuk mengurutkan array of string secara descending
-// mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortAscending = (name) => {
+        return name.sort()
+    }
+    // TODO: Fungsi untuk mengurutkan array of string secara descending
+    // mengembalikan array yang sudah diurutkan
+const sortDescending = (name) => {
+    return name.sort().reverse()
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
+    console.log(sorter ?.(names, sortAscending) ?.join("\n"));
+    console.log(sorter ?.(names, sortDescending) ?.join("\n"));
 })();
 
 module.exports = {
-  sorter,
-  sortAscending,
-  sortDescending,
-  names,
+    sorter,
+    sortAscending,
+    sortDescending,
+    names,
 };
